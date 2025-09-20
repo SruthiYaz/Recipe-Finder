67fb24315fe667401a0bcc4a40bc2a4d707504aa
Sruthi Y. <156405263+SruthiYaz@users.noreply.github.com>
2025-09-20 23:05:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,9 @@ const sampleRecipes = [
 
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.width === "250px") {
-    sidebar.style.width = "0";
-  } else {
-    sidebar.style.width = "250px";
-  }
+  sidebar.classList.toggle("open");
 }
+
 
 // Dark mode toggle
 darkModeToggle.addEventListener("click", () => {
